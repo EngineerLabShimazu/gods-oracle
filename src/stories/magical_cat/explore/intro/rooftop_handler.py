195e3fcddd4c2f2f7820a039570c122c66a9ffad
GenkiTech <shimazu.g.r@gmail.com>
2020-10-20 21:18:23 +0900

    feat: end_flagを回収していない場合の屋上

--- src/stories/magical_cat/explore/intro/rooftop_handler.py
+++ src/stories/magical_cat/explore/intro/rooftop_handler.py
@@ -14,7 +14,13 @@ class RooftopIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         speech_text = """
-        屋上ですね！かしこまりました！
+        勇者「屋上ですね！かしこまりました！」
+        <audio src="soundbank://soundlibrary/human/amzn_sfx_person_running_03"/>
+        <audio src="soundbank://soundlibrary/doors/doors_wood/wood_06"/>
+        盲目の生徒「すまない。耳をすましているが、何も聞こえない。。。」
+        勇者「そうか。何かあったら教えてくれ。
+        <break time="1s"/> 
+        神よ、次はどうすればよろしいでしょうか？」
         """
         handler_input.response_builder.speak(
             speech_text).set_should_end_session(False)
