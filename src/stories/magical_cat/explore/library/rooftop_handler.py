e55c5c5770c1cdb8f958325b36cdec864d0a5b8f
GenkiTech <shimazu.g.r@gmail.com>
2020-10-22 21:49:53 +0900

    feat: 闘技場、図書館から、手がかりなしで屋上へ行った場合のテキスト追加

--- src/stories/magical_cat/explore/library/rooftop_handler.py
+++ src/stories/magical_cat/explore/library/rooftop_handler.py
@@ -15,12 +15,19 @@ class RooftopIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         speech_text = """
-        探索図書館の屋上インテントだよ！
+        勇者「屋上ですね！かしこまりました！」
+        <audio src="soundbank://soundlibrary/human/amzn_sfx_person_running_03"/>
+        <audio src="soundbank://soundlibrary/doors/doors_wood/wood_06"/>
+        盲目の生徒「すまない。耳をすましているが、何も聞こえない。。。」
+        勇者「そうか。何かあったら教えてくれ。
+        <break time="1s"/> 
+        神よ、次はどうすればよろしいでしょうか？
         """
         handler_input.response_builder.speak(
             speech_text).set_should_end_session(False)
 
         session = handler_input.attributes_manager.session_attributes
         session['oracle_limit'] = session['oracle_limit'] - 1
+        session['scene'] = 'explore.rooftop'
 
         return handler_input.response_builder.response
