b7d6e16d8fbce85de4c9c30f6e9f2f60738c77e2
GenkiTech <shimazu.g.r@gmail.com>
2020-10-22 21:02:52 +0900

    feat: 闘技場から図書館へ行く

--- src/stories/magical_cat/explore/colosseum/add_handlers.py
+++ src/stories/magical_cat/explore/colosseum/add_handlers.py
@@ -1,8 +1,11 @@
 from util.skill_builder import sb
 from .rooftop_handler import RooftopIntentHandler
 from .colosseum_handler import ColosseumIntentHandler
+from .library_handler import LibraryIntentHandler
 
-for h in [RooftopIntentHandler,
-          ColosseumIntentHandler,
+for h in [
+    RooftopIntentHandler,
+    ColosseumIntentHandler,
+    LibraryIntentHandler
           ]:
     sb.add_request_handler(h())
