ce73bddc18e1eaf73b63a5aa5edeec4ed783cef9
GenkiTech <shimazu.g.r@gmail.com>
2020-10-26 17:03:26 +0900

    FOF-113 feat: Add ReAsk intro

--- src/stories/magical_cat/intro.py
+++ src/stories/magical_cat/intro.py
@@ -75,6 +75,7 @@ class MagicalCatIntent(AbstractRequestHandler):
         session = handler_input.attributes_manager.session_attributes
         session['oracle_limit'] = 5
         session['scene'] = 'explore.intro'
+        session['re_ask'] = ' 勇者「神よ！私はどうすればよいでしょうか？」'
 
         return handler_input.response_builder.response
 
