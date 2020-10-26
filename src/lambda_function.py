3e526c381a98d5a6637d951858b78a29c88f261c
GenkiTech <shimazu.g.r@gmail.com>
2020-10-26 20:39:12 +0900

    feat: Add end_flag.colosseum 🤣🤣🤣

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -183,6 +183,7 @@ import stories.magical_cat.explore.library.add_handlers
 import stories.magical_cat.explore.colosseum.add_handlers
 import stories.magical_cat.explore.rooftop.add_handlers
 import stories.magical_cat.end_flag.library.add_handlers
+import stories.magical_cat.end_flag.colosseum.add_handlers
 
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
