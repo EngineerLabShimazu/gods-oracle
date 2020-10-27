e4f6df5b11ceaa2714de7c7881e2a252b6d43241
GenkiTech <shimazu.g.r@gmail.com>
2020-10-27 20:57:34 +0900

    FOF-11 feat: 画面に選択肢表示 intro library

--- src/stories/magical_cat/explore/intro/library_handler.py
+++ src/stories/magical_cat/explore/intro/library_handler.py
@@ -1,5 +1,7 @@
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_intent_name
+from ask_sdk_model import ui
+from util import assets
 
 
 class LibraryIntentHandler(AbstractRequestHandler):
@@ -26,5 +28,23 @@ class LibraryIntentHandler(AbstractRequestHandler):
         session['scene'] = 'explore.library'
         session['oracle_limit'] = session['oracle_limit'] - 1
         session['re_ask'] = '勇者「役に立ちそうな本はどれだろう、、、？」'
+
+        image_url = assets.get_image(
+            'humans/hero/hero_stand_512')
+        handler_input.response_builder.set_card(
+            ui.StandardCard(
+                title='勇者「手がかりとなるものはどれだろう、、、？」',
+                text='・妖精のかくれんぼ\r\n'
+                     '・スキル「シノビアシ」\r\n'
+                     '・魔法の猫「プレシャ」\r\n'
+                     '・図書館へ行く\r\n'
+                     '・闘技場へ行く\r\n'
+                     '・屋上へ行く',
+                image=ui.Image(
+                    small_image_url=image_url,
+                    large_image_url=image_url
+                )
+            )
+        )
 
         return handler_input.response_builder.response
--- src/stories/magical_cat/explore/library/colosseum_handler.py
+++ src/stories/magical_cat/explore/library/colosseum_handler.py
@@ -1,5 +1,7 @@
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
 from ask_sdk_core.utils import is_intent_name
+from ask_sdk_model import ui
+from util import assets
 
 
 class ColosseumIntentHandler(AbstractRequestHandler):
@@ -15,7 +17,7 @@ class ColosseumIntentHandler(AbstractRequestHandler):
         speech_text = """
         勇者「闘技場ですね！かしこまりました！」
         <audio src="soundbank://soundlibrary/human/amzn_sfx_person_running_03"/>
-        勇者「闘技場に到着いたしました、、、手がかりとなるものはどれだろう、、、？」
+        勇者「闘技場に到着いたしました、、、手がかりはどこにあるだろう、、、？」
         """
         handler_input.response_builder.speak(
             speech_text).set_should_end_session(False)
@@ -23,6 +25,24 @@ class ColosseumIntentHandler(AbstractRequestHandler):
         session = handler_input.attributes_manager.session_attributes
         session['scene'] = 'explore.colosseum'
         session['oracle_limit'] = session['oracle_limit'] - 1
-        session['re_ask'] = '勇者「手がかりとなるものはどれだろう、、、？」'
+        session['re_ask'] = '勇者「手がかりはどこにあるだろう、、、？」'
+
+        image_url = assets.get_image(
+            'humans/hero/hero_stand_512')
+        handler_input.response_builder.set_card(
+            ui.StandardCard(
+                title='勇者「手がかりはどこにあるだろう、、、？」',
+                text='・客席\r\n'
+                     '・倉庫\r\n'
+                     '・控室\r\n'
+                     '・図書館へ行く\r\n'
+                     '・闘技場へ行く\r\n'
+                     '・屋上へ行く',
+                image=ui.Image(
+                    small_image_url=image_url,
+                    large_image_url=image_url
+                )
+            )
+        )
 
         return handler_input.response_builder.response
